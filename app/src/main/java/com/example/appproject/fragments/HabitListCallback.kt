c47cea11b54b76af5d343204dcca09a81a84db57
IlyaRyzhenkov <ilya.rigenkov2@gmail.com>
2022-05-11 13:54:20 +0500

    removed old project module

--- app/src/main/java/com/example/appproject/fragments/HabitListCallback.kt
+++
@@ -1,7 +0,0 @@
-package com.example.appproject.fragments
-
-import com.example.appproject.Habit
-
-interface HabitListCallback {
-    fun onItemClicked(habit: Habit, position : Int)
-}
--- app/src/main/java/com/example/appproject/fragments/HabitsEditorCallback.kt
+++
@@ -1,9 +0,0 @@
-package com.example.appproject.fragments
-
-import com.example.appproject.Habit
-
-interface HabitsEditorCallback {
-    fun onHabitCreated(habit: Habit, position: Int)
-
-    fun onHabitEdited(oldHabit: Habit, newHabit: Habit, oldPosition: Int)
-}
